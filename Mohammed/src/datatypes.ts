16266d58c3879fb177bb1a1b077b1c89117f514d
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-02-15 18:17:55 +0000

    Add arrays

---
+++ Mohammed/src/datatypes.ts
@@ -0,0 +1,15 @@
+// Array of different types
+let array1: (string | number)[] = ['hello', 'world', 45]
+let array2: (string | number | boolean)[] = [false, 'hello', 'world', 45]
+
+// Nested arrays
+let array3: number[][] = [
+    [1, 2],
+    [3, 4]
+]
+
+// Nested arrays with normal
+let array4: (number[] | number)[] = [
+    [1, 2],
+    5
+]
