e6dc7cddd3d3868266eb234f299ac1e68100fea0
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-02-17 23:26:58 +0000

    Do basic types challenges

---
+++ Mohammed/src/challenges.ts
@@ -0,0 +1,32 @@
+// Basic Types
+/*
+// *-*-*-*-*-*-*-*-*-*-* Challenge 1 ------------------
+// Create a variable with the type number and assign it an arbitrary value
+
+let num: number = 33
+
+// *-*-*-*-*-*-*-*-*-*-* Challenge 2 ------------------
+// Create a variable with the type string and use the addition operator to put two arbitrary words together
+
+let str: string = 'Hello' + 'World'
+
+//  *-*-*-*-*-*-*-*-*-*-* Challenge 3 ------------------
+// Simulate a Type Error by assigning a number  to a string
+
+str = 23
+
+// *-*-*-*-*-*-*-*-*-*-* Challenge 4 ------------------
+// Create a variable with the type Boolean and assign it to the logical OR operator comparing two values
+
+let bool: boolean = true || false
+
+// *-*-*-*-*-*-*-*-*-*-* Challenge 5 ------------------
+// Simulate a Type Error by assigning a number to a boolean
+
+bool = 55
+
+// *-*-*-*-*-*-*-*-*-*-* Challenge 6 ------------------
+// Get the length of an arbitrary string
+
+let strLength = str.length
+*/
