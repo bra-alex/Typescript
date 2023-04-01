a147fd7f008322c670a2108fe01059de4662077b
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-04-01 17:17:32 +0000

    Add custom logger

--- node/typescript-with-mongoose/src/app.ts
+++ node/typescript-with-mongoose/src/app.ts
@@ -1,12 +1,13 @@
 import config from 'config'
 import express from 'express'
 
+import logger from './utils/logger'
 import mongoConnect from './utils/connect'
 
 const app = express()
 const PORT = config.get<number>('port')
 
 app.listen(PORT, async () => {
-  console.log('Running')
+  logger.info(`App is running at http://localhost:${PORT}`)
   await mongoConnect()
 })
--- node/typescript-with-mongoose/src/utils/connect.ts
+++ node/typescript-with-mongoose/src/utils/connect.ts
@@ -1,13 +1,14 @@
 import config from 'config'
+import logger from './logger'
 import mongoose from 'mongoose'
 
 export default async function mongoConnect() {
   const dbUri = config.get<string>('dbUri')
   try {
     await mongoose.connect(dbUri)
-    console.log('DB connected')
+    logger.info('DB connected')
   } catch (error) {
-    console.error(error)
+    logger.error(error)
     process.exit(1)
   }
 }
