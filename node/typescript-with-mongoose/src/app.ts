bc68152f89c7bc38f5e14931c581ba23b13a0deb
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-04-01 17:24:39 +0000

    Add route

--- node/typescript-with-mongoose/src/app.ts
+++ node/typescript-with-mongoose/src/app.ts
@@ -1,6 +1,7 @@
 import config from 'config'
 import express from 'express'
 
+import routes from './routes'
 import logger from './utils/logger'
 import mongoConnect from './utils/connect'
 
@@ -10,4 +11,6 @@ const PORT = config.get<number>('port')
 app.listen(PORT, async () => {
   logger.info(`App is running at http://localhost:${PORT}`)
   await mongoConnect()
+
+  routes(app)
 })
