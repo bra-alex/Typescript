02dc770511c818fb3499953e187668afce4f7c9e
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-04-02 10:53:22 +0000

    Add sessions route

--- node/typescript-with-mongoose/src/routes.ts
+++ node/typescript-with-mongoose/src/routes.ts
@@ -2,9 +2,13 @@ import { Express, Request, Response } from 'express'
 
 import { createUserSchema } from './schema/user.schema'
 import validateResource from './middleware/validateResource'
+import { createSessionSchema } from './schema/session.schema'
 import { createUserHandler } from './controller/user.controller'
+import { createUserSessionHandler } from './controller/session.controller'
 
 export default function routes(app: Express) {
-  app.post('/api/users', validateResource(createUserSchema), createUserHandler)
   app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200))
+
+  app.post('/api/users', validateResource(createUserSchema), createUserHandler)
+  app.post('/api/sessions', validateResource(createSessionSchema), createUserSessionHandler)
 }
