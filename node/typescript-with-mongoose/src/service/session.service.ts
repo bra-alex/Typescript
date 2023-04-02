7c19f6eebc24f4f95fdb5a5d93b70046edcc1f1b
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-04-02 11:13:24 +0000

    Add getSessions to session service

--- node/typescript-with-mongoose/src/service/session.service.ts
+++ node/typescript-with-mongoose/src/service/session.service.ts
@@ -1,7 +1,14 @@
-import SessionModel from '../models/session.model'
+import { FilterQuery } from 'mongoose'
+import SessionModel, { SchemaDocument } from '../models/session.model'
 
-export async function createSession(userId: string, userAgent: string) {
+async function createSession(userId: string, userAgent: string) {
   const session = await SessionModel.create({ user: userId, userAgent })
 
   return session.toJSON()
 }
+
+async function getSessions(query: FilterQuery<SchemaDocument>) {
+  return SessionModel.find(query).lean()
+}
+
+export { createSession, getSessions }
