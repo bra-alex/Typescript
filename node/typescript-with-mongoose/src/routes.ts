c0e5bd1bb2d96aa99085d159dd6cfac1d1f08874
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-04-03 10:22:22 +0000

    Add requiredUser middleware

--- node/typescript-with-mongoose/src/routes.ts
+++ node/typescript-with-mongoose/src/routes.ts
@@ -1,5 +1,6 @@
 import { Express, Request, Response } from 'express'
 
+import requireUser from './middleware/requireUser'
 import { createUserSchema } from './schema/user.schema'
 import validateResource from './middleware/validateResource'
 import { createSessionSchema } from './schema/session.schema'
@@ -8,7 +9,7 @@ import { createUserSessionHandler, getUserSessionsHandler } from './controller/s
 
 export default function routes(app: Express) {
   app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200))
-  app.get('/api/sessions', getUserSessionsHandler)
+  app.get('/api/sessions', requireUser, getUserSessionsHandler)
 
   app.post('/api/users', validateResource(createUserSchema), createUserHandler)
   app.post('/api/sessions', validateResource(createSessionSchema), createUserSessionHandler)
