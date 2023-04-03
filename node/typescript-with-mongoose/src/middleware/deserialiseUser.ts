88dc1d42c98e77cd89c8f3324b07c2de1525100d
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-04-03 10:20:55 +0000

    Update deserialiseUser

--- node/typescript-with-mongoose/src/middleware/deserialiseUser.ts
+++ node/typescript-with-mongoose/src/middleware/deserialiseUser.ts
@@ -14,6 +14,7 @@ const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
     res.locals.user = decoded
     return next()
   }
+  return next()
 }
 
 export default deserializeUser
