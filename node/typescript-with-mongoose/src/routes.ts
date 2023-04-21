93f4bfd109a0e71f6ca6221a37c1e1329ae53b7f
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-04-21 21:55:43 +0000

    Add products route

--- node/typescript-with-mongoose/src/routes.ts
+++ node/typescript-with-mongoose/src/routes.ts
@@ -10,13 +10,37 @@ import {
   deleteSessionHandler,
   getUserSessionsHandler,
 } from './controller/session.controller'
+import {
+  deleteProductHandler,
+  getProductHandler,
+  updateProductHandler,
+} from './controller/product.controller'
+import {
+  createProductSchema,
+  deleteProductSchema,
+  getProductSchema,
+  updateProductSchema,
+} from './schema/product.schema'
 
 export default function routes(app: Express) {
-  app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200))
-  app.get('/api/sessions', requireUser, getUserSessionsHandler)
-
   app.post('/api/users', validateResource(createUserSchema), createUserHandler)
-  app.post('/api/sessions', validateResource(createSessionSchema), createUserSessionHandler)
 
+  app.get('/api/sessions', requireUser, getUserSessionsHandler)
+  app.post('/api/sessions', validateResource(createSessionSchema), createUserSessionHandler)
   app.delete('/api/sessions', requireUser, deleteSessionHandler)
+
+  app.get('/api/products/:productId', validateResource(getProductSchema), getProductHandler)
+  app.post('/api/products', [requireUser, validateResource(createProductSchema)], createUserHandler)
+  app.put(
+    '/api/products/:productId',
+    [requireUser, validateResource(updateProductSchema)],
+    updateProductHandler,
+  )
+  app.put(
+    '/api/products/:productId',
+    [requireUser, validateResource(deleteProductSchema)],
+    deleteProductHandler,
+  )
+
+  app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200))
 }
---
+++ node/typescript-with-mongoose/src/service/product.service.ts
@@ -0,0 +1,28 @@
+import { FilterQuery, QueryOptions, UpdateQuery } from 'mongoose'
+
+import ProductModel, { ProductDocument, ProductInput } from '../models/product.model'
+
+async function createProduct(productDetails: ProductInput) {
+  return await ProductModel.create(productDetails)
+}
+
+async function findProduct(
+  query: FilterQuery<ProductDocument>,
+  options: QueryOptions = { lean: true },
+) {
+  return await ProductModel.findOne(query, {}, options)
+}
+
+async function findAndUpdateProduct(
+  query: FilterQuery<ProductDocument>,
+  update: UpdateQuery<ProductDocument>,
+  options: QueryOptions,
+) {
+  return ProductModel.findOneAndUpdate(query, update, options)
+}
+
+async function deleteProduct(query: FilterQuery<ProductDocument>) {
+  return ProductModel.deleteOne(query)
+}
+
+export { createProduct, findProduct, findAndUpdateProduct, deleteProduct }
